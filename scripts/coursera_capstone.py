4a7793322862439d419c197f0039ec5f172645b1
Sanjeev George <stg2031991@gmail.com>
2020-07-18 18:49:28 +0200

    Jst added print code

--- scripts/coursera_capstone.py
+++ scripts/coursera_capstone.py
@@ -2,4 +2,4 @@ import pandas as pd
 import numpy as np
 
 <h1 align="center"><font size="5">Battle of the Neighborhoods</font></h1>
-This code will be used for the Coursera Capstone Project
+print("This code will be used for the Coursera Capstone Project")
