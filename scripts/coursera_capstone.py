a6e8214e63420374023db7898efc909e8ca2c1df
Sanjeev George <stg2031991@gmail.com>
2020-07-18 18:57:57 +0200

    Changed

--- scripts/coursera_capstone.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-print("Hello Capstone Project Course!")
-#<h1 align="center"><font size="5">Battle of the Neighborhoods</font></h1>
-print("Hello Capstone Project Course!")
