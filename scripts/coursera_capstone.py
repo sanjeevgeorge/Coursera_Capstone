efde104dfc9f67d06145cfba9eda2f64bad321dc
Sanjeev George <stg2031991@gmail.com>
2020-07-18 18:51:39 +0200

    More print

--- scripts/coursera_capstone.py
+++ scripts/coursera_capstone.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
 
-<h1 align="center"><font size="5">Battle of the Neighborhoods</font></h1>
-print("This code will be used for the Coursera Capstone Project")
+print("Hello Capstone Project Course!")
+#<h1 align="center"><font size="5">Battle of the Neighborhoods</font></h1>
+print("Hello Capstone Project Course!")
